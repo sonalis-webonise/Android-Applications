f8d89a191461f9780c378ac43e85afbd0dbca39d
sonalis-webonise <sssonali63@gmail.com>
2016-09-29 14:14:45 +0530

    Changes made

--- Calculator/app/src/main/java/com/example/webonise/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/webonise/calculator/MainActivity.java
@@ -13,67 +13,67 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    EditText editText1;
-    EditText editText2;
-    RadioButton radioButtonAdd;
-    RadioButton radioButtonSubtract;
-    RadioButton radioButtonMultiply;
-    RadioButton radioButtonDivide;
-    RadioGroup radioGroup;
-    Button btnCalculate;
-    TextView txtViewResult;
+    public final static String toastMessage = "Operation Done";
+    private EditText editText1;
+    private EditText editText2;
+    private RadioButton radioButtonAdd;
+    private RadioButton radioButtonSubtract;
+    private RadioButton radioButtonMultiply;
+    private RadioButton radioButtonDivide;
+    private RadioGroup radioGroup;
+    private Button btnCalculate;
+    private TextView txtViewResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initViews();
+        initTextViews();
+        initButtonViews();
+        initRadioViews();
     }
 
-    private void initViews(){
-        editText1=(EditText)findViewById(R.id.editText1);
-        editText2=(EditText)findViewById(R.id.editText2);
+    private void initRadioViews() {
+        radioButtonAdd = (RadioButton) findViewById(R.id.radioButtonAdd);
+        radioButtonSubtract = (RadioButton) findViewById(R.id.radioButtonSubtract);
+        radioButtonMultiply = (RadioButton) findViewById(R.id.radioButtonMultiply);
+        radioButtonDivide = (RadioButton) findViewById(R.id.radioButtonDivide);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+    }
 
-        txtViewResult=(TextView)findViewById(R.id.txtViewResult);
+    private void initTextViews() {
+        editText1 = (EditText) findViewById(R.id.editText1);
+        editText2 = (EditText) findViewById(R.id.editText2);
+        txtViewResult = (TextView) findViewById(R.id.txtViewResult);
+    }
 
-        radioButtonAdd=(RadioButton)findViewById(R.id.radioButtonAdd);
-        radioButtonSubtract=(RadioButton)findViewById(R.id.radioButtonSubtract);
-        radioButtonMultiply=(RadioButton)findViewById(R.id.radioButtonMultiply);
-        radioButtonDivide=(RadioButton)findViewById(R.id.radioButtonDivide);
-
-        RadioGroup radioGroup=(RadioGroup)findViewById(R.id.radioGroup);
-
-        btnCalculate=(Button)findViewById(R.id.btnCalculate);
-
+    private void initButtonViews() {
+        btnCalculate = (Button) findViewById(R.id.btnCalculate);
         btnCalculate.setOnClickListener(this);
     }
-
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-
             case R.id.btnCalculate:
-            double result;
-            double number1, number2;
-            number1 = Double.parseDouble(editText1.getText().toString());
-            number2 = Double.parseDouble(editText2.getText().toString());
+                double result;
+                double number1, number2;
+                number1 = Double.parseDouble(editText1.getText().toString());
+                number2 = Double.parseDouble(editText2.getText().toString());
 
-            Toast toast = Toast.makeText(getApplicationContext(), "Operation done", Toast.LENGTH_LONG);
-            toast.show();
+                Toast toast = Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_LONG);
+                toast.show();
                 if (radioButtonAdd.isChecked()) {
-                result = number1 + number2;
-                txtViewResult.setText(String.valueOf(result));
-                }
-                else if(radioButtonSubtract.isChecked()){
-                    result=number1-number2;
+                    result = number1 + number2;
                     txtViewResult.setText(String.valueOf(result));
-                }
-                else if(radioButtonMultiply.isChecked()){
-                    result=number1*number2;
+                } else if (radioButtonSubtract.isChecked()) {
+                    result = number1 - number2;
                     txtViewResult.setText(String.valueOf(result));
-                }
-                else if(radioButtonDivide.isChecked()){
-                    result=number1/number2;
+                } else if (radioButtonMultiply.isChecked()) {
+                    result = number1 * number2;
+                    txtViewResult.setText(String.valueOf(result));
+                } else if (radioButtonDivide.isChecked()) {
+                    result = number1 / number2;
                     txtViewResult.setText(String.valueOf(result));
                 }
                 break;
